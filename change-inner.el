5741ae143af4e2766a1092c9f515aea8c4d8c733
Magnar Sveen <magnars@gmail.com>
2012-07-26 20:43:28 +0200

    Update commentary.

--- change-inner.el
+++ change-inner.el
@@ -20,36 +20,40 @@
 
 ;;; Commentary:
 
-;; change-inner gives you vim's `ci"` command, building on
+;; # change-inner.el
+;;
+;; change-inner gives you vim's `ci` command, building on
 ;; [expand-region](https://github.com/magnars/expand-region.el). It is most easily
 ;; explained by example:
-
-;;    function test() {
-;;      return "semantic kill";
-;;    }
-
-;; With point after the word semantic
-
+;;
+;;     function test() {
+;;       return "semantic kill";
+;;     }
+;;
+;; With point after the word `semantic`
+;;
 ;;  * `change-inner "` would kill the contents of the string
 ;;  * `change-outer "` would kill the entire string
 ;;  * `change-inner {` would kill the return-statement
 ;;  * `change-outer {` would kill the entire block
-
-;; I use M-i/o for this.
-
+;;
+;; I use `M-i` and `M-o` for this.
+;;
+;; Giving these commands a prefix argument `C-u` means copy instead of kill.
+;;
 ;; ## Installation
-
+;;
 ;; Start by installing
 ;; [expand-region](https://github.com/magnars/expand-region.el).
-
+;;
 ;;     (require 'change-inner)
 ;;     (global-set-key (kbd "M-i") 'change-inner)
 ;;     (global-set-key (kbd "M-o") 'change-outer)
-
-;; ## Contribute
-
-;; change-inner is a thin wrapper around expand-region. Most fixes to
-;; change-inner belong there.
+;;
+;; ## It's not working in my favorite mode
+;;
+;; That may just be because expand-region needs some love for your mode. Please
+;; open a ticket there: https://github.com/magnars/expand-region.el
 
 ;;; Code:
 
